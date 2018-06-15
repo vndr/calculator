693dba54ecd8d023cdb903619e2433927f28becb
Juraj Vondrak <juraj@unomee.com>
2018-06-15 10:41:44 +0100

    unit test typo fix

--- src/test/java/com/vndr/calculator/CalculatorTest.java
+++ src/test/java/com/vndr/calculator/CalculatorTest.java
@@ -1,6 +1,6 @@
 package com.vndr.calculator;
 import org.junit.Test;
-import static org.junit.Assert.assertEqueals;
+import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
     private Calculator calculator = new Calculator();
