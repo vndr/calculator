97ab183e69909f24ed612866d4db3a777d016ab5
Juraj Vondrak <juraj@unomee.com>
2018-06-15 11:13:12 +0100

    Add static code analysis

--- src/main/java/com/vndr/calculator/Calculator.java
+++ src/main/java/com/vndr/calculator/Calculator.java
@@ -1,6 +1,7 @@
 package com.vndr.calculator;
 import org.springframework.stereotype.Service;
 
+/** Calculator logic */
 @Service
 public class Calculator {
     int sum(int a, int b) {
--- src/main/java/com/vndr/calculator/CalculatorApplication.java
+++ src/main/java/com/vndr/calculator/CalculatorApplication.java
@@ -3,6 +3,7 @@ package com.vndr.calculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/** Calculatorapplication logic */
 @SpringBootApplication
 public class CalculatorApplication {
 
